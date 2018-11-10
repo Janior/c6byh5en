8f5c5129da9e7d3a1d48cca7507912e383fb356b
Janior <janior_orlando@hotmail.com>
2018-11-10 14:37:36 -0800

    Incluir Opcion editar y borrar

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,26 +1,43 @@
 module Api 
-    module V1 
-      class Api::V1::ProductsController < ApplicationController 
-        protect_from_forgery with: :null_session 
-        def index 
-          @products = Product.all 
-          render json: @products 
-        end 
- 
-        def create 
-          product = Product.new(products_params) 
-          if product.save 
-            render json: @product, status: 201 
-          else 
-            render json: { errors: product.errors }, status: 422 
-          end 
-        end 
 
-
-        private 
-        def products_params 
-          params.require(:product).permit(:name, :price) 
+  module V1 
+    class Api::V1::ProductsController < ApplicationController 
+      protect_from_forgery with: :null_session 
+      def index 
+        @products = Product.all 
+        render json: @products 
+      end 
+  
+      def create 
+        product = Product.new(products_params) 
+        if product.save 
+          render json: @product, status: 201 
+        else 
+          render json: { errors: product.errors }, status: 422 
         end 
       end 
+
+      def update 
+         @product = Product.find(params[:id]) 
+        if(products_params[:name].present? && products_params[:price].present?) 
+          @product.update(products_params) 
+          render json: @product, status: 200 
+        else 
+          puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" 
+          render json: {errors:"Error 422"}, status: 422 
+        end 
+      end 
+  
+  
+      def destroy 
+        @product = Product.find(params[:id]) 
+        @product.destroy 
+      end 
+
+      private 
+      def products_params 
+        params.require(:product).permit(:name, :price) 
+      end 
     end 
+  end 
 end 
